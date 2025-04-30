1455a81919e80cafee083423dddd56f4bfa9ef93
unknown <Administrador@LABF530128.fuvs.br>
2025-04-29 22:17:37 -0300

    criando uma pagina completa

--- primeiro-projeto-react/src/App.jsx
+++ primeiro-projeto-react/src/App.jsx
@@ -1,8 +1,23 @@
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <p className="texto-vermelho">Marcos Antonio dos Santos</p>
+    <div className="container">
+      <h1>Marcos Antonio dos Santos</h1>
+      <p className="descricao-pessoal">Desenvolvedor, músico e adestrador de cães.</p>
+
+      <h2 className="hobbies">Hobbies</h2>
+      <ul>
+        <li>Música</li>
+        <li>Treinamento de cães</li>
+      </ul>
+
+      <h2 className="skills">Skills</h2>
+      <ol>
+        <li>React</li>
+        <li>JS</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>NodeJS</li>
+      </ol>
     </div>
   )
 }
