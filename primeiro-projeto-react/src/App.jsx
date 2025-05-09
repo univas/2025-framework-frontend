8687ea3d24f9e8c576f0acfd6ef7a248da55ac81
unknown <Administrador@LABF530128.fuvs.br>
2025-05-08 21:32:40 -0300

    aula de css modules

--- primeiro-projeto-react/src/App.jsx
+++ primeiro-projeto-react/src/App.jsx
@@ -1,23 +1,31 @@
+import hobbies_styles from './hobbies.module.css'
+import skills_styles from './skills.module.css'
+import titulo_styles from './titulo.module.css'
+
 function App() {
   return (
     <div className="container">
-      <h1>Marcos Antonio dos Santos</h1>
+      <h1 className={titulo_styles.titulo}>Marcos Antonio dos Santos</h1>
       <p className="descricao-pessoal">Desenvolvedor, músico e adestrador de cães.</p>
 
-      <h2 className="hobbies">Hobbies</h2>
-      <ul>
-        <li>Música</li>
-        <li>Treinamento de cães</li>
-      </ul>
+      <div className={hobbies_styles.container}>
+        <h2 className={hobbies_styles.titulo}>Hobbies</h2>
+        <ul>
+          <li>Música</li>
+          <li>Treinamento de cães</li>
+        </ul>
+      </div>
 
-      <h2 className="skills">Skills</h2>
-      <ol>
-        <li>React</li>
-        <li>JS</li>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>NodeJS</li>
-      </ol>
+      <div className={skills_styles.container}>
+        <h2 className={skills_styles.titulo}>Skills</h2>
+        <ol>
+          <li>React</li>
+          <li>JS</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>NodeJS</li>
+        </ol>
+      </div>
     </div>
   )
 }
