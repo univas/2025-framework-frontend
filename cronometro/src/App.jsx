a84f96cfbdbe8a1314ed8cc0da1a42034afbd5c3
unknown <Administrador@LABF530128.fuvs.br>
2025-05-13 21:36:13 -0300

    usando use effect com api

--- cronometro/src/App.jsx
+++ cronometro/src/App.jsx
@@ -2,19 +2,33 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [segundos, setSegundos] = useState(0)
+  // const [segundos, setSegundos] = useState(0)
+
+  // useEffect(() => {
+  //   const timer = setInterval(() => {
+  //     setSegundos((valorAnterior) => valorAnterior + 1)
+  //   }, 1000)
+
+  //   return () => clearInterval(timer)
+  // }, [])
+
+
+  const [piada, setPiada] = useState("")
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      setSegundos((valorAnterior) => valorAnterior + 1)
-    }, 1000)
-
-    return () => clearInterval(timer)
+    fetch('https://api.chucknorris.io/jokes/random')
+    .then(res => res.json()) // traz a resposta da api
+    .then(dados => { // converte o conteúdo da resposta pra json
+      setPiada(dados.value)
+    })
+    .catch(err => setPiada("Erro ao carregar dados..."))
   }, [])
 
   return (
     <>
-      <h1>Tempo decorrido: {segundos}</h1>
+      {/* <h1>Tempo decorrido: {segundos}</h1> */}
+      <h1>Piada Aleatória:</h1>
+      <p>{piada == "" ? "Aguardando carregar..." : piada}</p>
     </>
   )
 }
